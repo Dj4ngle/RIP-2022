5cea936a57cf1bfc4ffc66693507404f5492bbfc
junglekiller4 <41653468+junglekiller4@users.noreply.github.com>
2022-09-27 21:34:27 +0300

    some updates...

--- lab2/printing/printwebsite/admin.py
+++ lab2/printing/printwebsite/admin.py
@@ -8,7 +8,15 @@ from mptt.admin import MPTTModelAdmin
 #
 # # admin.site.register(models.List_3D, MPTTModelAdmin)
 
-admin.site.register(models.Image3d)
-admin.site.register(models.Model3d)
+@admin.register(models.Model3d)
+class Model3DAdmin(admin.ModelAdmin):
+    list_display = ["name", "price", "image_path"]
+
+@admin.register(models.Image3d)
+class Model3DAdmin(admin.ModelAdmin):
+    list_display = ["title", "image"]
+
+#admin.site.register(models.Image3d)
+#admin.site.register(models.Model3d)
 
 
